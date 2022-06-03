0d1e798daa29b02718207e98847badf6d58e175f
shinoda <81030617+shinodashx@users.noreply.github.com>
2022-06-03 16:58:10 +0800

    Add the function of deck v1.1

--- cwk2/src/main/java/comp1721/cwk2/Deck.java
+++ cwk2/src/main/java/comp1721/cwk2/Deck.java
@@ -1,3 +1,55 @@
 package comp1721.cwk2;
 
-// Implement Deck class here
+import java.util.Collections;
+
+/**
+ * Class representing the deck of cards
+ *
+ * <p>The class is used to represent a deck of cards</p>
+ */
+
+public class Deck extends CardCollection {
+
+    /**
+     * Creates a new deck of cards
+     */
+    public Deck() {
+        super();
+        for (Card.Suit suit : Card.Suit.values()) {
+            for (Card.Rank rank : Card.Rank.values()) {
+                this.add(new Card(rank, suit));
+            }
+        }
+    }
+
+    /**
+     * Return  the number of cards in the deck
+     *
+     * @return t the number of cards in the deck
+     */
+    public int size() {
+        return cards.size();
+    }
+
+    /**
+     * Check if the deck is empty or not
+     *
+     * @return true if the deck is empty of cards, false otherwise
+     */
+    public boolean isEmpty() {
+        return cards.isEmpty();
+    }
+
+    /**
+     * Check if the  specified card contains in the deck.
+     *
+     * @param card Card is looked for
+     * @return true if the deck contains the specified card, false otherwise.
+     */
+    public boolean contains(Card card) {
+        return cards.contains(card);
+    }
+
+
+
+}
