23929babe99d11533817890199e4335a08160bf7
shinoda <81030617+shinodashx@users.noreply.github.com>
2022-06-03 17:00:17 +0800

    Update PokerHand.java
    Add the function of pokerhand and with init v 2.1

--- cwk2/src/main/java/comp1721/cwk2/PokerHand.java
+++ cwk2/src/main/java/comp1721/cwk2/PokerHand.java
@@ -1,3 +1,46 @@
 package comp1721.cwk2;
 
-// Implement PokerHand class here
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Collections;
+
+/**
+ * Class representing the poker hand.
+ *
+ * <p> The class is used to represent a poker hand</p>
+ */
+
+public class PokerHand extends CardCollection {
+
+
+    /**
+     * Define a hashmap
+     */
+    private final Map<Character, Integer> map = new HashMap<>();
+
+    /**
+     * Creates a new poker hand.
+     */
+    public PokerHand() {
+        super();
+    }
+
+    /**
+     * Creates a new poker hand from a list of cards.
+     *
+     * @param cards List of cards.
+     * @throws CardException if the list of cards is invalid.
+     */
+    public PokerHand(String cards) {
+        super();
+        String[] cardArray = cards.split(" ");
+        for (String card : cardArray) {
+            this.add(new Card(card));
+        }
+    }
+
+}
